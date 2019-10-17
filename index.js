943327673d4190254bba6e3d9b518e8c2d90aec2
varsha121 <36448167+varsha121@users.noreply.github.com>
2019-10-17 15:05:15 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var side1=5;
+var side2=6;
+var side3=7;
+var perimeter=(side1+side2+side3)/2;
+var area=Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3)));
+console.log(area);
